019dcbe883bea7023c9e6fa401974bfbcb02b72e
HARIKRISHNAN S R <sivan.hari.sr@gmail.com>
2021-05-09 01:32:10 +0530

    updated

--- entry.js
+++ entry.js
@@ -2,6 +2,10 @@ const dataHelper = require("./helpers/data.helper");
 
 const configHelper = require("./helpers/config.helper");
 
+const express = require("express");
+const app = express();
+const port = 3000;
+
 (async () => {
   console.log("config", configHelper);
   try {
@@ -14,3 +18,11 @@ const configHelper = require("./helpers/config.helper");
   }
   //await dataHelper.checkAvalability();
 })();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
